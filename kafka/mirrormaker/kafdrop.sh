fb6e945eff735f562e880beedebc82a37892b8d8
JavierB Bernal <javi@MacBook-Pro-de-JavierB.local>
2022-01-21 13:17:07 +0100

    correct parameters

--- kafka/mirrormaker/kafdrop.sh
+++ kafka/mirrormaker/kafdrop.sh
@@ -6,7 +6,7 @@ mv linux-amd64/helm /root/helm
 git clone https://github.com/obsidiandynamics/kafdrop && cd kafdrop
 
 ../helm upgrade -i kafdrop-cp4na chart --set kafka.brokerConnect=iaf-system-kafka-bootstrap.network-orchestration.svc.cluster.local:9092 --set server.servlet.contextPath="/"
-oc expose svc kafdrop
+oc expose svc kafdrop-cp4na
 
-../helm upgrade -i kafdrop chart --set kafka.brokerConnect=mano-kafka-cluster-bootstrap.kafka-cluster-operator.svc.cluster.local:9092 --set server.servlet.contextPath="/" --set service.nodeport="30901"
+../helm upgrade -i kafdrop chart --set kafka.brokerConnect=mano-kafka-cluster-bootstrap.kafka-cluster-operator.svc.cluster.local:9092 --set server.servlet.contextPath="/" --set service.nodePort="30901"
 oc expose svc kafdrop
