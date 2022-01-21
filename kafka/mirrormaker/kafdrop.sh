ba856a165fb69f00e07e427744a69ce93fe0b9fa
JavierB Bernal <javi@MacBook-Pro-de-JavierB.local>
2022-01-21 12:41:17 +0100

    adding kafdrop script

---
+++ kafka/mirrormaker/kafdrop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /root/
+curl https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz -o helm.tar.gz
+tar -xf helm.tar.gz
+mv linux-amd64/helm /root/helm
+git clone https://github.com/obsidiandynamics/kafdrop && cd kafdrop
+
+../helm upgrade -i kafdrop-cp4na chart --set kafka.brokerConnect=iaf-system-kafka-bootstrap.network-orchestration.svc.cluster.local:9092 --set server.servlet.contextPath="/"
+oc expose svc kafdrop
+
+../helm upgrade -i kafdrop chart --set kafka.brokerConnect=mano-kafka-cluster-bootstrap.kafka-cluster-operator.svc.cluster.local:9092 --set server.servlet.contextPath="/" --set service.nodeport="30901"
+oc expose svc kafdrop
